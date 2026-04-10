45b0418fe07a58fa718306a4bfcd03f41083c25c
yathisha-coder <yathisai85@gmail.com>
2026-04-10 12:02:09 -0400

    Added the lines of code.

--- src/main/java/org/example/Calculator1.java
+++ src/main/java/org/example/Calculator1.java
@@ -15,11 +15,37 @@ public class Calculator1 {
 
         //Here, we are asking the user to enter the interest rate
         System.out.println("Enter the interest rate percentage : % ");
-        //here, we are using double because the interest rate we are converting percentage to the decimal
+        //Here, we are using double because the interest rate we are converting percentage to the decimal
         double interestRate = sc.nextDouble();
         //showing the interest rate to the user
         System.out.println("Your interest rate: %  " +interestRate);
 
+        //Here, we are asking the user to enter the loan length in years
+        System.out.println("Enter the number of years: ");
+        //Here, we are using int because the loan length duration should be whole number
+        int years = sc.nextInt();
+        //Showing how many years of loan duration
+        System.out.println("How many years of loan: " +years);
+
+        //Here, Converting the values
+        //Calculating the monthly interest
+        double monthlyInterest = (interestRate/100)/12;
+        //Calculating the total months
+        int totalMonths = (years*12);
+        //Showing the results that after converting the values
+        System.out.println("Monthly Interest: " + monthlyInterest);
+        System.out.println("Total Months: "+ totalMonths);
+
+        //Calculating the monthly payment
+        double monthlyPayment = principal*(monthlyInterest*Math.pow(1+monthlyInterest,totalMonths)/(Math.pow(1+monthlyInterest,totalMonths)-1));
+        //Showing the results
+        System.out.println("Monthly Payment: $"+monthlyPayment);
+
+        //Calculate the total interest
+        double total = monthlyPayment * totalMonths;
+        double totalInterest = total - principal;
+        //Showing the results
+        System.out.println("Total Interest: $" + totalInterest);
 
     }
 }
